2aa9b3a7c13b0c02a4290d1425e6b9819f914b51
Thomas Nabet <thomas.nabet@gmail.com>
2021-08-30 15:50:49 +0200

    cosmetic fix

--- src/mvc/public/actions/update.php
+++ src/mvc/public/actions/update.php
@@ -3,26 +3,36 @@
  * Describe what it does to show you're not that dumb!
  *
  **/
+use bbn\X;
 
 /** @var $ctrl \bbn\Mvc\Controller */
 $ctrl->obj->success = false;
 if ( isset($ctrl->post['text'], $ctrl->post['id']) ){
   $cfg = $ctrl->inc->options->getParentCfg($ctrl->post['id']);
-  if ( !empty($cfg['schema']) && ($schema = json_decode($cfg['schema'], true)) ){
-    foreach ( $ctrl->post as $i => $d ){
-      if (
-        (($idx = \bbn\X::find($schema, ['field' => $i])) !== null) &&
-        isset($schema[$idx]['type']) &&
-        (strtolower($schema[$idx]['type']) === 'json')
-      ){
-        $ctrl->post[$i] = json_decode($d, true);
+  if (!empty($cfg['schema'])) {
+    if (is_array($cfg['schema'])) {
+      $schema = $cfg['schema'];
+    }
+    elseif (is_string($cfg['schema'])) {
+      $schema = json_decode($cfg['schema'], true);
+    }
+
+    if (!empty($schema)) {
+      foreach ( $ctrl->post as $i => $d ){
+        if (
+          (($idx = X::find($schema, ['field' => $i])) !== null) &&
+          isset($schema[$idx]['type']) &&
+          (strtolower($schema[$idx]['type']) === 'json')
+        ){
+          $ctrl->post[$i] = json_decode($d, true);
+        }
       }
     }
   }
 
   if ( $ctrl->obj->res = $ctrl->inc->options->set($ctrl->post['id'], $ctrl->post) ){
     $ctrl->obj->success = true;
-    $ctrl->obj->data = \bbn\X::mergeArrays($ctrl->inc->options->nativeOption($ctrl->post['id']), $ctrl->inc->options->option($ctrl->post['id']));
+    $ctrl->obj->data = X::mergeArrays($ctrl->inc->options->nativeOption($ctrl->post['id']), $ctrl->inc->options->option($ctrl->post['id']));
     //$ctrl->obj->data = $ctrl->inc->options->nativeOption($ctrl->post['id']);
     $ctrl->obj->post = $ctrl->post;
   }
