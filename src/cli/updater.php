5c66f3973f38807dfa87f097bdfed4811ef74bd8
nabab <thomas.nabet@gmail.com>
2022-04-04 05:30:45 +0200

    Using bbn\appui in updater

--- src/cli/updater.php
+++ src/cli/updater.php
@@ -1,8 +1,15 @@
 <?php
 
 use bbn\X;
+
 /** @var $ctrl \bbn\mvc\ctrl */
 X::adump("Hello from the updater...");
+$appui = new bbn\Appui();
+if ($res = $appui->importOptions()) {
+  echo $res . _("options changed");
+}
+
+/*
 $fs = new \bbn\File\System();
 $file0 = BBN_LIB_PATH.'bbn/bbn/changes.json';
 if (!is_file($file0)) {
@@ -26,45 +33,4 @@ foreach ($changes as $ch) {
   }
 }
 
-$file = BBN_LIB_PATH.'bbn/bbn/options.json';
-if (!is_file($file)) {
-  X::adump("Hello from the updater...");
-  throw new \Exception("Impossible to find the file options");
-}
-X::adump("File options found...");
-$appui_options = X::toArray($fs->decodeContents($file));
-$root = $ctrl->inc->options->getDefault();
-$res = 0;
-if ($res = $ctrl->inc->options->import($appui_options, $root)) {
-  X::adump($res." options changed");
-  if ($res = (int)$ctrl->inc->options->updatePlugins()) {
-    X::adump($res." options from plugins changed");
-  }
-  if ($res = (int)$ctrl->inc->options->updateAllTemplates()) {
-    X::adump($res." options from templates changed");
-  }
-}
-
-/*
-$permissions_options = X::toArray($fs->decodeContents($file2));
-$plugins_options = X::toArray($fs->decodeContents($file3));
-
-array_unshift($appui_options[0]['items'], $permissions_options);
-$permissions_options['id_alias'] = ['permissions', 'appui'];
-$permissions_options['items'][0]['id_alias'] = ['access', 'permissions', 'appui'];
-$permissions_options['items'][1]['id_alias'] = ['plugins', 'permissions', 'appui'];
-$permissions_options['items'][2]['id_alias'] = ['options', 'permissions', 'appui'];
-foreach ($appui_options[0]['items'] as $i => &$it) {
-  if ($i) {
-    if (!isset($it['items'])) {
-      $it['items'] = [];
-    }
-    $it['items'][] = $plugins_options;
-    $it['items'][] = $permissions_options;
-  }
-}
-unset($it);
-if ($res = $ctrl->inc->options->import($appui_options, $root)) {
-  echo $res." options changed";
-}
 */
