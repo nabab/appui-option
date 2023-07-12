cea7457440669045bbda2bcf93a68847ad2c2b48
nabab <thomas.nabet@gmail.com>
2023-07-12 17:44:03 +0200

    Option import

--- src/mvc/model/actions/import.php
+++ src/mvc/model/actions/import.php
@@ -5,7 +5,7 @@ $res = ['success' => false];
 if (
   isset($model->data['id'], $model->data['option']) &&
   ($data = json_decode($model->data['option'], true)) &&
-  ($o = $model->inc->options->import($data, $model->data['id']))
+  ($o = $model->inc->options->importAll($data, $model->data['id']))
 ){
   $res['success'] = $o;
 }
