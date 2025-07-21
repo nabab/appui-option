e0498a412b88002c8a60579954c02fb95a478adb
nabab <thomas.nabet@gmail.com>
2025-07-22 01:13:31 +0200

    Typo

--- src/components/new/subplugin/subplugin.js
+++ src/components/new/subplugin/subplugin.js
@@ -1,6 +1,13 @@
 (() => {
   return {
     mixins: [bbn.cp.mixins['appui-option-tree']],
-    
+    data() {
+      return {
+        currentPlugin: null
+      }
+    },
+    created() {
+      this.currentPlugin = this.closest('bbn-container').getComponent().currentPlugin;
+    },
   };
 })()
